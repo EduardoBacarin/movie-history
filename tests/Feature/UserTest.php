1ea3932d575fd10cf9258768c7cb28180836d4c3
Eduardo Bacarin <eduardo.obacarin@gmail.com>
2024-09-17 21:18:29 -0300

    Fixing Feature Tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,31 +10,28 @@ use Tests\TestCase;
 
 class UserTest extends TestCase {
 
-    public function test_create_user(): void {
-        User::where(["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5"])->delete();
-        $data = [
-            "name" => "John Doe",
-            "email" => "john@doe.com",
-            "password" => "123qwe"
-        ];
-        $response = $this->post('/api/auth/register', $data);
-        $response->assertStatus(201);
-        $response->assertJson(["success" => true, "code" => 201]);
-        $this->assertDatabaseHas("users", ["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5", "name" => "John Doe", "email" => "john@doe.com"]);
+    private $data = [
+        "name" => "John Doe",
+        "email" => "john@doe.com",
+        "password" => "123qwe"
+    ];
+    public function setUp(): void{
+        parent::setUp();
         User::where(["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5"])->delete();
     }
 
+    public function test_create_user(): void {
+        $response = $this->post('/api/auth/register', $this->data);
+        $response->assertStatus(201);
+        $response->assertJson(["success" => true, "code" => 201]);
+        $this->assertDatabaseHas("users", ["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5", "name" => "John Doe", "email" => "john@doe.com"]);
+    }
+
     public function test_login_user(): void {
-        User::where(["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5"])->delete();
-        $data = [
-            "name" => "John Doe",
-            "email" => "john@doe.com",
-            "password" => "123qwe"
-        ];
-        User::create($data);
+        User::create($this->data);
         $loginRequest = [
-            "email" => $data['email'],
-            "password" => $data['password'],
+            "email" => $this->data['email'],
+            "password" => $this->data['password'],
         ];
         $response = $this->post('/api/auth/login', $loginRequest);
         $response->assertStatus(201);
@@ -42,20 +39,13 @@ class UserTest extends TestCase {
         $this->assertNotEmpty($response->json('token'));
         $getToken = Redis::get("token:".$response->json('token'));
         $this->assertNotEmpty($getToken);
-        User::where(["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5"])->delete();
     }
 
     public function test_logout_user(): void {
-        User::where(["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5"])->delete();
-        $data = [
-            "name" => "John Doe",
-            "email" => "john@doe.com",
-            "password" => "123qwe"
-        ];
-        User::create($data);
+        User::create($this->data);
         $loginRequest = [
-            "email" => $data['email'],
-            "password" => $data['password'],
+            "email" => $this->data['email'],
+            "password" => $this->data['password'],
         ];
         $response = $this->post('/api/auth/login', $loginRequest);
         $logout = $this->withToken($response->json('token'))->delete('/api/auth/logout');
@@ -63,6 +53,10 @@ class UserTest extends TestCase {
         $logout->assertStatus(200);
         $getToken = Redis::get("token:".$response->json('token'));
         $this->assertEmpty($getToken);
+    }
+
+    public function tearDown(): void{
         User::where(["_id" => "ecab04f45bd75691d932e0d8b96c9e07cbd737a207c57fe950fd46803fb383f5"])->delete();
+        parent::tearDown();
     }
 }
